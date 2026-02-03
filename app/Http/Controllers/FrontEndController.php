bab39d7681ce9d8d1d5dd23966140229ea13819f
karimul2001 <karimul.bd501@gmail.com>
2026-02-03 13:42:00 +0600

    first commit

---
+++ app/Http/Controllers/FrontEndController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Inertia\Inertia;
+
+class FrontEndController extends Controller
+{
+    public function Home()
+    {
+        return Inertia::render('Home');
+    }
+
+    public function About()
+    {
+        return Inertia::render('About');
+    }
+}
